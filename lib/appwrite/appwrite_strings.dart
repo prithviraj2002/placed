7c7f6fdc281dbb416fef855a55e1e0216733ea46
prithviraj_kapil <kapilprithviraj99@gmail.com>
2024-11-21 12:24:23 +0530

    Update appwrite_strings.dart
    Hiding api keys.

--- lib/appwrite/appwrite_strings.dart
+++ lib/appwrite/appwrite_strings.dart
@@ -1,19 +1,19 @@
 class AppWriteStrings{
-  static const String endPoint = 'https://cloud.appwrite.io/v1';
-  static const String projectId = '659f955654db1b66e4b6';
+  static const String endPoint = '';
+  static const String projectId = '';
 
   //Database
-  static const String dbID = '65ab73cee9265f8b049b';
-  static const String profileCollectionsId = '65ab73eec953f243cdac';
-  static const String jobsCollectionId = '65ad06fdc3b55e37ae8b';
-  static const String broadcastCollectionId = '65aca87fbf43f6aef2bb';
+  static const String dbID = '';
+  static const String profileCollectionsId = '';
+  static const String jobsCollectionId = '';
+  static const String broadcastCollectionId = '';
 
   //Storage
-  static const String imageBucketId = '65b35832b94925a129da';
-  static const String resumeBucketId = '65b108a4e427ecd7669c';
-  static const String deptBucketId = '65e59e475d2b35953495';
+  static const String imageBucketId = '';
+  static const String resumeBucketId = '';
+  static const String deptBucketId = '';
 
   //urls
-  static const String resumeViewUrl = '$endPoint/storage/buckets/$resumeBucketId/files/65d843736885561b6a1d/view?project=$projectId&mode=admin';
-  static const String imageViewUrl = '$endPoint/storage/buckets/$imageBucketId/files/65d843736885561b6a1d/view?project=$projectId&mode=admin';
+  static const String resumeViewUrl = '';
+  static const String imageViewUrl = '';
 }
